abfc205b5833220bf3c0a2814e9947277b10b73a
augusto <augustosaaude@gmail.com>
2023-08-02 17:58:56 -0300

    feat: create interfaces to Infra

---
+++ src/Manager.Infra/Interfaces/IUserRepository.cs
@@ -0,0 +1,10 @@
+﻿using Manager.Domain.Entities;
+
+namespace Manager.Infra.Interfaces;
+
+public interface IUserRepository : IBaseRepository<User>
+{
+    Task<User> GetByEmail(string email);
+    Task<List<User>> SearchByEmail(string email);
+    Task<List<User>> SearchByNome(string nome);
+}
